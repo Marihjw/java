434fc93341184a1c288030f54630ecb8a24dc8de
Marihjw <diasmariane2008@gmail.com>
2024-03-19 15:10:54 -0300

    novo arquivo conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+ const numero = 456;
+ const numeroString= "456";
+
+ //number()
+ //string()
+
+ console.log(numero + Number( numeroString))
+
+ // conversao explicita 
